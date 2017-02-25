e5fa65f2720d37e784dab6f1d2b1304b5111f5cf
InternetUnexplorer <ferncactus@gmail.com>
2017-02-24 19:45:25 -0800

    Added minimal main.c, to be filled in later.

---
+++ main.c
@@ -0,0 +1,16 @@
+/* main.c -- argument parsing and main logic of the compiler
+ *
+ * Copyright (C) 2017 Alex White <internetunexplorer@gmail.com>
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */ 
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	int i;
+	for(i = 1; i < argc; i++) {
+		
+	}
+}
