a928e85a06fc103b77609f540e3969650c20e203
InternetUnexplorer <internetunexplorer@gmail.com>
2019-09-21 17:29:40 -0700

    Fix 'vec_clear' and improve 'vec_(insert|remove)'
    'vec_clear' has been fixed, and 'vec_insert' and 'vec_remove' now use
    memmove to move elements, which should slightly improve performance.
    Formatting was also tweaked slightly.

--- util/vec.c
+++ util/vec.c
@@ -1,6 +1,7 @@
 #include "vec.h"
 
 #include <stdbool.h>
+#include <string.h>
 
 static bool _resize(Vec *vec, size_t capacity) {
     if ((vec->items = realloc(vec->items, capacity * sizeof(void*))) != NULL)
@@ -18,16 +19,18 @@ Vec *vec_new(void) {
     return vec;
 }
 
-void vec_free(Vec* vec) {
+void vec_free(Vec *vec) {
     vec_clear(vec);
     free(vec);
 }
 
-void vec_clear(Vec* vec) {
-    _resize(vec, 0);
+void vec_clear(Vec *vec) {
+    while (vec->length > 0)
+        free(vec->items[--vec->length]);
+    _resize(vec, 2);
 }
 
-size_t vec_len(Vec* vec) {
+size_t vec_len(Vec *vec) {
     return vec->length;
 }
 
@@ -39,27 +42,25 @@ void vec_set(Vec *vec, size_t index, void *item) {
     vec->items[index] = item;
 }
 
-void vec_insert(Vec* vec, size_t index, void* item) {
+void vec_insert(Vec *vec, size_t index, void *item) {
     if (vec->length == vec->capacity)
         if (!_resize(vec, vec->capacity != 0 ? vec->capacity * 2 : 2))
             return;
-    for (size_t i = vec->length; i > index; i--)
-        vec->items[i] = vec->items[i - 1];
+    memmove(&vec->items[index + 1], &vec->items[index],
+            (vec->length++ - index) * sizeof(void*));
     vec->items[index] = item;
-    vec->length++;
 }
 
 void *vec_remove(Vec *vec, size_t index) {
     void *item = vec->items[index];
-    vec->length--;
-    for (size_t i = index; i < vec->length; i++)
-        vec->items[i] = vec->items[i + 1];
+    memmove(&vec->items[index], &vec->items[index + 1],
+            (--vec->length - index) * sizeof(void*));
     if (vec->length < vec->capacity / 4)
         _resize(vec, vec->capacity / 2);
     return item;
 }
 
-void vec_pushf(Vec* vec, void *item) {
+void vec_pushf(Vec *vec, void *item) {
     vec_insert(vec, 0, item);
 }
 
@@ -71,6 +72,6 @@ void *vec_popf(Vec *vec) {
     return vec->length != 0 ? vec_remove(vec, 0) : NULL;
 }
 
-void *vec_popb(Vec* vec) {
+void *vec_popb(Vec *vec) {
     return vec->length != 0 ? vec_remove(vec, vec->length - 1) : NULL;
 }
