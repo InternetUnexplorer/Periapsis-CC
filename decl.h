445b70a6a12b3f86040ca929392b597345812aca
InternetUnexplorer <internetunexplorer@gmail.com>
2019-09-27 17:19:32 -0700

    Update ops.h and decl.h
    Several new Binops were added, and 'UNOP_LNOT' was renamed to
    'UNOP_L_NOT'. Also, the entries in DeclSpec were reordered
    alphabetically to make things a bit neater.

--- decl.h
+++ decl.h
@@ -5,11 +5,11 @@
 
 typedef enum DeclSpec {
     DECL_SPEC_NONE = 0x0,
-    DECL_SPEC_VOID = 0x1,
+    DECL_SPEC_FLOAT = 0x1,
     DECL_SPEC_INT = 0x2,
-    DECL_SPEC_FLOAT = 0x4,
-    DECL_SPEC_SIGNED = 0x8,
-    DECL_SPEC_UNSIGNED = 0xF,
+    DECL_SPEC_SIGNED = 0x4,
+    DECL_SPEC_UNSIGNED = 0x8,
+    DECL_SPEC_VOID = 0xF,
 } DeclSpec;
 
 typedef uint8_t DeclSpecs;
--- ops.h
+++ ops.h
@@ -4,7 +4,7 @@
 typedef enum Unop {
     UNOP_NEG,      // Negation
     UNOP_NOT,      // Bitwise NOT
-    UNOP_LNOT,     // Logical NOT
+    UNOP_L_NOT,    // Logical NOT
     UNOP_PREINCR,  // Pre-increment
     UNOP_PREDECR,  // Pre-decrement
     UNOP_POSTINCR, // Post-increment
@@ -12,16 +12,23 @@ typedef enum Unop {
 } Unop;
 
 typedef enum Binop {
-    BINOP_MUL, // Multiplication
-    BINOP_DIV, // Division
-    BINOP_ADD, // Addition
-    BINOP_SUB, // Subtraction
-    BINOP_LT,  // Less than
-    BINOP_GT,  // Greater than
-    BINOP_LE,  // Less than or equal
-    BINOP_GE,  // Greater than or equal
-    BINOP_EQ,  // Equal
-    BINOP_NE,  // Not equal
+    BINOP_MUL,    // Multiplication
+    BINOP_DIV,    // Division
+    BINOP_ADD,    // Addition
+    BINOP_SUB,    // Subtraction
+    BINOP_LT,     // Less than
+    BINOP_GT,     // Greater than
+    BINOP_LE,     // Less than or equal
+    BINOP_GE,     // Greater than or equal
+    BINOP_EQ,     // Equal
+    BINOP_NE,     // Not equal
+    BINOP_AND,    // Bitwise AND
+    BINOP_XOR,    // Bitwise XOR
+    BINOP_OR,     // Bitwise OR
+    BINOP_L_AND,  // Logical AND
+    BINOP_L_OR,   // Logical OR
+    BINOP_ASSIGN, // Assignment
+    BINOP_COMMA,  // Evaluate both sides and discard LHS
 } Binop;
 
 #endif
